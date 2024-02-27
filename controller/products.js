5297fccfac7414697335054a35f54454d01e403c
Sujeet Kumar <sujeetc1202kc@gmail.com>
2024-02-27 18:38:54 +0530

    checkin the query object and then receiving data fron db

--- controller/products.js
+++ controller/products.js
@@ -5,8 +5,18 @@ const getAllProductsStatic = async (req, res) => {
   res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
-    const products = await Product.find(req.query);
-    res.status(200).json({ products, nbHits: products.length });
+  // to check query object is there or not
+  //  in this way is query is present there in Schema it will send result if it is not there it will send all the data
+  const { featured } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+
+  // const products = await Product.find(req.query);
+  const products = await Product.find(queryObject);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = { getAllProductsStatic, getAllProducts };
