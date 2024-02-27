2cd0f522a22c0c21b1631301500571626d7388d0
Sujeet Kumar <sujeetc1202kc@gmail.com>
2024-02-27 20:20:30 +0530

    Sort general setup

--- controller/products.js
+++ controller/products.js
@@ -1,7 +1,8 @@
 const Product = require("../models/products");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({});
+  const products = await Product.find({}).sort("-name price");
+  // soting based on name and price if we add - before name then it will give in descending order
   res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
