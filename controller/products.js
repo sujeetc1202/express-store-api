bb665fbc6f2119db58a712eb157614c85841d730
Sujeet Kumar <sujeetc1202kc@gmail.com>
2024-02-27 18:30:26 +0530

    qeryParams used for dymanically getting the data using params

--- controller/products.js
+++ controller/products.js
@@ -5,7 +5,8 @@ const getAllProductsStatic = async (req, res) => {
   res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "products route" });
+    const products = await Product.find(req.query);
+    res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = { getAllProductsStatic, getAllProducts };
