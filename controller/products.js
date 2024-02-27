18df4db269d27678e28c4fc5bf8759f3b068abfe
Sujeet Kumar <sujeetc1202kc@gmail.com>
2024-02-27 19:01:01 +0530

    handling company in query param

--- controller/products.js
+++ controller/products.js
@@ -7,15 +7,20 @@ const getAllProductsStatic = async (req, res) => {
 const getAllProducts = async (req, res) => {
   // to check query object is there or not
   //  in this way is query is present there in Schema it will send result if it is not there it will send all the data
-  const { featured } = req.query;
+  const { featured, company } = req.query;
   const queryObject = {};
 
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
   }
 
-  // const products = await Product.find(req.query);
+  if (company) {
+    queryObject.company = company;
+  }
+
   const products = await Product.find(queryObject);
+
+  //   const products = await Product.find(req.query);
   res.status(200).json({ products, nbHits: products.length });
 };
 
