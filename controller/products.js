3ea0c7301baabc4b58e9fb7e2072e20027744f3e
Sujeet Kumar <sujeetc1202kc@gmail.com>
2024-02-27 22:08:34 +0530

    pagination setup with page and limit

--- controller/products.js
+++ controller/products.js
@@ -1,11 +1,9 @@
 const Product = require("../models/products");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({})
-    .sort("-name, price")
-    .select("name price")
-    .limit(10) // used to limit the list size
-    .skip(5); // used to skip from the list
+  const products = await Product.find({}).sort("name").select("name price");
+  // .limit(10) // used to limit the list size
+  // .skip(5); // used to skip from the list
   // soting based on name and price if we add - before name then it will give in descending order
   res.status(200).json({ products, nbHits: products.length });
 };
@@ -39,6 +37,15 @@ const getAllProducts = async (req, res) => {
     const fieldsList = fields.split(",").join(" ");
     result = result.select(fieldsList);
   }
+
+  // pagination
+  const page = Number(req.query.page) || 1;
+  // limit
+  const limit = Number(req.query.limit) || 10;
+
+  const skip = (page - 1) * limit;
+  // updating the result as per limit/pagination
+  result = result.skip(skip).limit(limit);
 
   const products = await result;
   //   const products = await Product.find(req.query);
