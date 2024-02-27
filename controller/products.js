7890a5ba1f1b2b5980aea63d6a4415970831dd9e
Sujeet Kumar <sujeetc1202kc@gmail.com>
2024-02-27 21:41:43 +0530

    select fields implementation

--- controller/products.js
+++ controller/products.js
@@ -1,14 +1,14 @@
 const Product = require("../models/products");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).sort("-name price");
+  const products = await Product.find({}).select("name price");
   // soting based on name and price if we add - before name then it will give in descending order
   res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
   // to check query object is there or not
   //  in this way is query is present there in Schema it will send result if it is not there it will send all the data
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -23,14 +23,20 @@ const getAllProducts = async (req, res) => {
   }
 
   let result = Product.find(queryObject);
-
+  // sort
   if (sort) {
     const sortList = sort.split(",").join(" ");
     result = result.sort(sortList);
   } else {
     result = result.sort("createdAt");
   }
-  const products = await await result;
+  // select
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+
+  const products = await result;
   //   const products = await Product.find(req.query);
   res.status(200).json({ products, nbHits: products.length });
 };
